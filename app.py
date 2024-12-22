463cec0b7e981ecd9861dd8099ad88be049f9644
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2024-12-22 13:19:23 +0100

    Update app.py - update explanation in the upper part of the app

--- app.py
+++ app.py
@@ -153,8 +153,11 @@ description = dbc.Col(
                                 ),
                                 dbc.Col(
                                     html.P(
-                                        "This is an example of interactive economic data plotter. "  # noqa: E501
-                                        " "
+                                        "This is an example of an interactive economic data plotter that allows users to visualize two datasets through bar plots and scatter plots. "  # noqa: E501
+                                        "Users can enhance their scatter plots by adding linear trendlines, providing valuable insights into data trends and relationships. "  # noqa: E501
+                                        "The application offers customizable color schemes, enabling users to tailor the visual appearance of their plots to better suit their presentation needs. "  # noqa: E501
+                                        "Additionally, users have the option to download their visualizations in multiple formats, including PNG, JPG, and SVG, ensuring compatibility with various applications and platforms. "  # noqa: E501
+                                        "This tool aims to facilitate data analysis and improve the accessibility of economic data visualization for users of all skill levels. "  # noqa: E501
                                     ),
                                     md=True,
                                 ),
